3de2c343e3353b8b08b6559e0a813f9b0bd3383a
Dania <alda24hj@student.ju.se>
2024-11-13 11:25:01 +0100

    game.js

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(800, 800);
 }
-
 function draw() {
-  background(255, 140, 0);
+  //game starting screen
+  background(143, 240, 255);
+  
+  //text
+  textAlign(CENTER);
+  textSize(70);
+  textStyle(BOLD);
+  fill(255);
+  text("Bunny Lander", width / 2, height / 2);
+
+ //button
+ fill(255, 149, 0);
+ noStroke();
+ rect(280, 485, 200, 100);
 }
