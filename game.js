d530c7da85433ba7187dd2595f7d8537d94150d0
Dania <alda24hj@student.ju.se>
2024-11-15 13:27:09 +0100

    added the texts for the start page, struggling to make it visually appealing. Not that I don't have ideas, but they're not coming out great

--- game.js
+++ game.js
@@ -1,19 +1,42 @@
+// let font;
+// function preload() {
+//   font = loadFont('PressStart2P-Regular.ttf');
+// }
+
 function setup() {
   createCanvas(800, 800);
 }
+
 function draw() {
   //game starting screen
   background(143, 240, 255);
-  
+
   //text
+  stroke(255, 0, 0);
+  strokeWeight(2);
+  textFont("Helvetica");
   textAlign(CENTER);
-  textSize(70);
+  textSize(60);
   textStyle(BOLD);
   fill(255);
-  text("Bunny Lander", width / 2, height / 2);
+  text("Bunny Lander", 400, 400);
 
- //button
- fill(255, 149, 0);
- noStroke();
- rect(280, 485, 200, 100);
+  function heart(x, y, s) {
+    fill(255, 0, 0);
+    beginShape();
+    vertex(390, 490);
+    bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+    bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+    endShape(CLOSE);
+    }
+//text on button
+fill(0);
+textFont("Helvetica");
+  textAlign(CENTER);
+  textSize(30);
+  textStyle(BOLD);
+text("Start", 393, 500);
+
+
+  
 }
